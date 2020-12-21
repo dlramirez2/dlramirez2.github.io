e1476de5fcdf13874420c6f2fcfb538f3f4e81a0
Diana Ramirez <dlramirez2@miners.utep.edu>
2020-12-20 22:00:34 -0700

    Update jsfile.js

--- jsfile.js
+++ jsfile.js
@@ -13,3 +13,19 @@
                 document.body.scrollTop = 0;
                 document.documentElement.scrollTop = 0;
             }
+
+            //collapsible functionality
+            var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
